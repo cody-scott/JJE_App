6ab9c45fc2e9d826ab03cba04fc4058467d62a45
namur007 <namur007@users.noreply.github.com>
2017-06-14 18:20:04 -0400

    bugs

--- JJE_App/settings.py
+++ JJE_App/settings.py
@@ -28,7 +28,7 @@ DEBUG = False
 
 # ALLOWED_HOSTS = ['127.0.0.1']
 # ALLOWED_HOSTS = ["*"]
-ALLOWED_HOSTS = ['jje-league.herokuapp.com', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['jje-league.herokuapp.com', '127.0.0.1', '0.0.0.0', 'locahost']
 # COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
 # Application definition
 
