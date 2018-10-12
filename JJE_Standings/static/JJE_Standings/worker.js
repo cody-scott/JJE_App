8d1028a39c83777fc6742aeb2704b63b42d145cd
Cody Scott <jcodyscott@gmail.com>
2018-10-12 15:01:28 -0400

    standings

--- JJE_Standings/static/JJE_Standings/worker.js
+++ JJE_Standings/static/JJE_Standings/worker.js
@@ -1,7 +1,7 @@
 var base_url = location["protocol"] + "//" + location["host"] + "/";
 var standings_api = base_url + "api" + "/";
 var current_standings_url = standings_api + "standings/";
-var all_standings_url = standings_api + "/all_standings/";
+var all_standings_url = standings_api + "all_standings/";
 
 var colors = [
     '#FF6666',
@@ -17,8 +17,11 @@ var colors = [
     '#FF66FF',
     '#FF66B2'
 ];
+var all_colors = colors;
 
 var standings_data;
+var all_standings_data;
+var week_labels;
 function get_standings() {
     $.getJSON(
         current_standings_url + "?format=json", function(data) {
@@ -31,7 +34,8 @@ function get_standings() {
 function get_all_standings() {
     $.getJSON(
         all_standings_url + "?format=json", function(data) {
-            standings_data = process_data(data);
+            all_standings_data = process_all_standings(data);
+            create_season_chart();
         }
     )
 }
@@ -42,10 +46,6 @@ function process_data(data) {
     var lbl = [];
     for (var i=0; i < data.length; i++) {
         var current_team = data[i];
-        // dt.push({
-        //     'x': current_team['team']['team_name'],
-        //     'y': current_team["stat_point_total"]
-        // });
         lbl.push(current_team['team']['team_name']);
         dt.push(current_team['stat_point_total'])
     }
@@ -59,10 +59,56 @@ function process_data(data) {
     return out_data;
 }
 
+function sortNumber(a,b) {
+    return a - b;
+}
+
+function process_all_standings(data) {
+    var out_dct = {};
+    var week_list = [];
+    for (var i=0; i < data.length; i++) {
+        var c_row = data[i];
+        var tm = c_row['team']['team_name'];
+        var standings_week_int = parseInt(c_row["standings_week"]);
+        var standings_points_int = parseInt(c_row["stat_point_total"]);
+        if (!out_dct[tm]) {
+            out_dct[tm] = [];
+        }
+        out_dct[tm].push(
+            {x: "Week " + standings_week_int, y: standings_points_int}
+        );
+        if (week_list.indexOf(standings_week_int) < 0) {
+            week_list.push(standings_week_int)
+        }
+        week_list.sort(sortNumber);
+    }
+    week_labels = week_list.map(function(x) { return ["Week " + x]});
+    var values = [];
+    var tmp_keys = [];
+
+    for (var key in out_dct) {
+        tmp_keys.push(key);
+    }
+    tmp_keys.sort();
+
+    for (var i=0; i<tmp_keys.length; i++) {
+        var key = tmp_keys[i];
+        var tmp = {
+            label: key,
+            data: out_dct[key],
+            fill: false,
+            borderColor: colors[i],
+            backgroundColor: colors[i]
+        };
+        values.push(tmp);
+    }
+
+    return values;
+}
 
 var myChart;
 function create_chart() {
-    var ctx = document.getElementById("myChart");
+    var ctx = document.getElementById("current_standings_chart");
 
     myChart = new Chart(ctx, {
         type: 'bar',
@@ -79,7 +125,7 @@ function create_chart() {
                     ticks: {
                         autoSkip: false
                     }
-                    }],
+                }],
             },
             legend: {
                 display: false
@@ -87,6 +133,33 @@ function create_chart() {
         }
     },);
 }
+
+function create_season_chart() {
+    var ctx = document.getElementById("season_chart");
+
+    var season_chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: week_labels,
+            datasets: all_standings_data
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }],
+                xAxes: [],
+            },
+            legend: {
+                display: true
+            },
+        }
+    },);
+}
+
+
 
 function check_chart_hidden() {
     var hidden = $(".canvas_column").attr('hidden');
@@ -137,20 +210,22 @@ function resize_checker() {
 
 function load_chart() {
     get_standings();
-                chart_loaded = true;
+    chart_loaded = true;
 }
 
 var chart_loaded = false;
 var is_mobile = false;
 
 $(document).ready(function() {
-    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    get_standings();
+    get_all_standings();
+    create_season_chart();
+    if( /Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
         is_mobile = true;
         resize_checker()
     }
     else {
         show_chart();
-        get_standings()
     }
 
     console.log(is_mobile);
